a6e1523c53d5cf7d31f6f56f294e0378f5971030
Keegan Leitz <keegan@openbay.com>
2019-08-31 21:37:47 -0400

    add eslint

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import invert from './invert';
 
-const foo: string = 'wait, dude';
+const foo: string = 'wait, dude'; // eslint-disable-line @typescript-eslint/no-inferrable-types
 
 console.log(`${foo}... is that ${invert(false)}?`);
