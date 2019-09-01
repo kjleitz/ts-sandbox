5db75c91caf428f8405606f8375815131bb0f32b
Keegan Leitz <keegan@openbay.com>
2019-08-31 20:51:10 -0400

    add jivescript

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
 import invert from './invert';
+import JiveScript from 'jivescript';
 
 const foo: string = 'wait, dude';
 
 console.log(`${foo}... is that ${invert(false)}?`);
+
+const js = new JiveScript();
+(window as any).js = js;
+
+js.hear('yay', () => {
+  js.say('sweet');
+});
